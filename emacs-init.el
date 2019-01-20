af79d755febff059dd57d914bc59b129a149517b
Wiliam Kudaka <wiliamkudaka@gmail.com>
2019-01-20 14:19:56 -0200

    my emacs cfg

---
+++ emacs-init.el
@@ -0,0 +1,31 @@
+(require 'package)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (tsdh-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(defun my-org-confirm-babel-evaluate (lang body)
+  (not lang))
+(setq org-confirm-babel-evaluate 'my-org-confirm-babel-evaluate)
+
+
+(org-babel-do-load-languages 'org-babel-load-languages
+    '(
+        (shell . t)
+        (python . t)
+    )
+)
+
